05ba4f9f925102283fd67307306c7aa70f8e7ad9
Anzelah <jepkoechanzellah@gmail.com>
2023-07-21 11:35:39 -0700

    quicksort

--- sort.h
+++ sort.h
@@ -23,6 +23,9 @@ listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_int(int *a, int *b);
+void quick_sort_two(int *array, int first, int last);
+void quick_sort(int *array, size_t size);
+int partition(int *array, int first, int last);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 #endif
