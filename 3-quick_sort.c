a75a0f6dbbb1333bbb7c0036567296e61f502202
Anzelah <jepkoechanzellah@gmail.com>
2023-07-21 12:47:52 -0700

    use size when printing array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -3,6 +3,9 @@
 /**
  * partition - diide and conquer the elements
  * @size: size of the arrays
+ * @array: array holding integers
+ * @first: element on left side
+ * @last: element on left side
  *
  * Return: unsorted partitioned arrays
  */
@@ -17,23 +20,27 @@ int partition(int *array, ssize_t first, ssize_t last, size_t size)
 	{
 		if (array[j] <= pivot)
 		{
-			i++;	
+			i++;
 			swap_int(&array[i], &array[j]);
 			if (i != j)
+			{
 				print_array(array, size);
+			}
 		}
 	}
 	swap_int(&array[i + 1], &array[last]);
-	if i != j
+	if (i != j)
 		print_array(array, size);
 
 	return (i + 1);
 }
 
 /**
- * quick_sort -function to implement the Quick sort algorithm
+ * quick_sort_two - function to implement the Quick sort algorithm
  * @first: integer at the beginning
  * @last: last element
+ * @array: array holding elements
+ * @size: array size
  */
 
 void quick_sort_two(int *array, ssize_t first, ssize_t last, size_t size)
@@ -50,7 +57,8 @@ void quick_sort_two(int *array, ssize_t first, ssize_t last, size_t size)
 }
 
 /**
- * quick_sort - sorts an array of integers in ascending order using the Quick sort algorithm
+ * quick_sort - sorts an array of integers in
+ * ascending order using the Quick sort algorithm
  * @array: pointer to array of integers
  * @size: size of the array
  */
@@ -71,7 +79,7 @@ void quick_sort(int *array, size_t size)
 
 void swap_int(int *a, int *b)
 {
-        int temp = *a;
-        *a = *b;
-        *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
